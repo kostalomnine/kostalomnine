201584bb2e8a6de3198e2e1406b715c28c238335
kostalomnine <62370609+kostalomnine@users.noreply.github.com>
2021-01-16 19:33:30 +0700

    Add files via upload

---
+++ working_htaccess/19/index.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+echo 'index';
+var_dump($_SERVER['REQUEST_URI']);
+//var_dump($_GET['get']);
---
+++ working_htaccess/37/test/index.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+include 'test/elem.php';
+
+//echo 'index';
+//var_dump($_SERVER['REQUEST_URI']);
